4d137784dc88326e11b53bcf51403040c3bba0fb
Sean Washington <hello@seanwash.com>
2016-10-22 12:00:40 -0400

    Update footer style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,14 @@
 <template>
-  <footer class="mw8 pa4 mt4 bt f6">
-    <a class="dib link black dim b tracked" href="/">Sean Washington</a>
+  <footer class="mw9 center ph2 mt4 f6">
+    <div class="bt pv4">
+      <a class="dib link black dim b tracked" href="/">Sean Washington</a>
 
-    <ul class="mt2 ma0 pa0">
-      <li class="dib"><a class="link black dim" href="http://github.com/seanwash">GitHub</a></li>
-      <li class="dib"><a class="link black dim ml2" href="https://spec.fm/podcasts/does-not-compute">Spec.fm</a></li>
-      <li class="dib ml2"><a class="link black dim" href="http://twitter.com/seanwashbot">Twitter</a></li>
-    </ul>
+      <ul class="mt2 ma0 pa0">
+        <li class="dib"><a class="link black dim" href="http://github.com/seanwash">GitHub</a></li>
+        <li class="dib"><a class="link black dim ml2" href="https://spec.fm/podcasts/does-not-compute">Spec.fm</a></li>
+        <li class="dib ml2"><a class="link black dim" href="http://twitter.com/seanwashbot">Twitter</a></li>
+      </ul>
+    </div>
   </footer>
 </template>
 
